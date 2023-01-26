943c5e7c0f9cd774fa5c2e184f43b7580406bdaa
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-01-26 16:35:33 +0200

    v2

--- Test/UserControl1.xaml.cs
+++ Test/UserControl1.xaml.cs
@@ -27,12 +27,16 @@ using System.IO;
 
 namespace Test
 {
-    /// <summary>
-    /// Interaction logic for UserControl1.xaml
-    /// </summary>
+    
     public partial class UserControl1 : UserControl
     {
+
+
         public WorkflowDesigner WorkflowDesigner;
+
+        const String DefultWorkflowFilePath = @"DefaultWorkflows\defaultWorkflow.xaml";
+        public string WorkflowFilePath = @"DefaultWorkflows\defaultWorkflow.xaml";
+        
 
         public UserControl1()
         {
@@ -40,7 +44,6 @@ namespace Test
             this.RegisterMetadata();
             this.AddWorkflowDesigner();
             this.AddToolBox();
-            this.AddPropertyInspector();
         }
         private void RegisterMetadata()
         {
@@ -50,10 +53,19 @@ namespace Test
         {
             this.WorkflowDesigner = new WorkflowDesigner();
 
-            
 
-            this.WorkflowDesigner.Load(new Sequence());
+
+            if (!string.IsNullOrEmpty(WorkflowFilePath))
+            {
+
+                this.WorkflowDesigner.Load(WorkflowFilePath);
+
+            }
+           
+            
             this.workflowDesignerPanel.Content = this.WorkflowDesigner.View;
+            this.AddPropertyInspector();
+
 
         }
         private ToolboxControl GetToolboxControl()
@@ -64,75 +76,74 @@ namespace Test
 
 
 
-             //AppDomain.CurrentDomain.Load("System.Activities");
-             //AppDomain.CurrentDomain.Load("System.ServiceModel.Activities");
-             //AppDomain.CurrentDomain.Load("System.Activities.Core.Presentation");
+            //AppDomain.CurrentDomain.Load("System.Activities");
+            //AppDomain.CurrentDomain.Load("System.ServiceModel.Activities");
+            //AppDomain.CurrentDomain.Load("System.Activities.Core.Presentation");
 
 
 
 
             // get all loaded assemblies
-            IEnumerable<Assembly> appAssemblies = AppDomain.CurrentDomain.GetAssemblies().OrderBy(a => a.GetName().Name);
+            /* IEnumerable<Assembly> appAssemblies = AppDomain.CurrentDomain.GetAssemblies().OrderBy(a => a.GetName().Name);
 
-            // check if assemblies contain activities
-            int activitiesCount = 0;
-            foreach (Assembly activityLibrary in appAssemblies)
-            {
-                var wfToolboxCategory = new ToolboxCategory(activityLibrary.GetName().Name);
-                var actvities = from
-                                    activityType in activityLibrary.GetExportedTypes()
-                                where
-                                    (activityType.IsSubclassOf(typeof(Activity))
-                                    || activityType.IsSubclassOf(typeof(NativeActivity))
-                                    || activityType.IsSubclassOf(typeof(DynamicActivity))
-                                    || activityType.IsSubclassOf(typeof(ActivityWithResult))
-                                    || activityType.IsSubclassOf(typeof(AsyncCodeActivity))
-                                    || activityType.IsSubclassOf(typeof(CodeActivity))
-                                    || activityType == typeof(System.Activities.Core.Presentation.Factories.ForEachWithBodyFactory<Type>)
-                                    || activityType == typeof(System.Activities.Statements.FlowNode)
-                                    || activityType == typeof(System.Activities.Statements.State)
-                                    || activityType == typeof(System.Activities.Core.Presentation.FinalState)
-                                    || activityType == typeof(System.Activities.Statements.FlowDecision)
-                                    || activityType == typeof(System.Activities.Statements.FlowNode)
-                                    || activityType == typeof(System.Activities.Statements.FlowStep)
-                                    || activityType == typeof(System.Activities.Statements.FlowSwitch<Type>)
-                                    || activityType == typeof(System.Activities.Statements.ForEach<Type>)
-                                    || activityType == typeof(System.Activities.Statements.Switch<Type>)
-                                    || activityType == typeof(System.Activities.Statements.TryCatch)
-                                    || activityType == typeof(System.Activities.Statements.While))
-                                    && activityType.IsVisible
-                                    && activityType.IsPublic
-                                    && !activityType.IsNested
-                                    && !activityType.IsAbstract
-                                    && (activityType.GetConstructor(Type.EmptyTypes) != null)
-                                    && !activityType.Name.Contains('`')                                 //optional, for extra cleanup
-                                orderby
-                                    activityType.Name
-                                select
-                                    new ToolboxItemWrapper(activityType);
+             // check if assemblies contain activities
+             int activitiesCount = 0;
+             foreach (Assembly activityLibrary in appAssemblies)
+             {
+                 var wfToolboxCategory = new ToolboxCategory(activityLibrary.GetName().Name);
+                 var actvities = from
+                                     activityType in activityLibrary.GetExportedTypes()
+                                 where
+                                     (activityType.IsSubclassOf(typeof(Activity))
+                                     || activityType.IsSubclassOf(typeof(NativeActivity))
+                                     || activityType.IsSubclassOf(typeof(DynamicActivity))
+                                     || activityType.IsSubclassOf(typeof(ActivityWithResult))
+                                     || activityType.IsSubclassOf(typeof(AsyncCodeActivity))
+                                     || activityType.IsSubclassOf(typeof(CodeActivity))
+                                     || activityType == typeof(System.Activities.Core.Presentation.Factories.ForEachWithBodyFactory<Type>)
+                                     || activityType == typeof(System.Activities.Statements.FlowNode)
+                                     || activityType == typeof(System.Activities.Statements.State)
+                                     || activityType == typeof(System.Activities.Core.Presentation.FinalState)
+                                     || activityType == typeof(System.Activities.Statements.FlowDecision)
+                                     || activityType == typeof(System.Activities.Statements.FlowNode)
+                                     || activityType == typeof(System.Activities.Statements.FlowStep)
+                                     || activityType == typeof(System.Activities.Statements.FlowSwitch<Type>)
+                                     || activityType == typeof(System.Activities.Statements.ForEach<Type>)
+                                     || activityType == typeof(System.Activities.Statements.Switch<Type>)
+                                     || activityType == typeof(System.Activities.Statements.TryCatch)
+                                     || activityType == typeof(System.Activities.Statements.While))
+                                     && activityType.IsVisible
+                                     && activityType.IsPublic
+                                     && !activityType.IsNested
+                                     && !activityType.IsAbstract
+                                     && (activityType.GetConstructor(Type.EmptyTypes) != null)
+                                     && !activityType.Name.Contains('`')                                 //optional, for extra cleanup
+                                 orderby
+                                     activityType.Name
+                                 select
+                                     new ToolboxItemWrapper(activityType);
 
-                actvities.ToList().ForEach(wfToolboxCategory.Add);
+                 actvities.ToList().ForEach(wfToolboxCategory.Add);
 
-                if (wfToolboxCategory.Tools.Count > 0)
-                {
-                    toolboxControl.Categories.Add(wfToolboxCategory);
-                    activitiesCount += wfToolboxCategory.Tools.Count;
-                }
-            }
+                 if (wfToolboxCategory.Tools.Count > 0)
+                 {
+                     toolboxControl.Categories.Add(wfToolboxCategory);
+                     activitiesCount += wfToolboxCategory.Tools.Count;
+                 }
+             }*/
             //fixed ForEach
 
 
             //fixid activites
 
 
-            /*
+
 
             toolboxControl.Categories.Add(new ToolboxCategory("Basic Activities")
             {
                 new ToolboxItemWrapper(typeof(Sequence)),
                 new ToolboxItemWrapper(typeof(WriteLine)),
                 new ToolboxItemWrapper(typeof(Assign)),
-                new ToolboxItemWrapper(typeof(InvokeWebService))
             });
 
             toolboxControl.Categories.Add(new ToolboxCategory("Control Flow Activities")
@@ -169,7 +180,7 @@ namespace Test
                 new ToolboxItemWrapper(typeof(Rethrow)),
                 new ToolboxItemWrapper(typeof(Throw))
             });
-            */
+
 
             return toolboxControl;
         }
@@ -189,11 +200,100 @@ namespace Test
         private void TabItem_GotFocus_RefreshXamlBox(object sender, RoutedEventArgs e)
         {
 
-            this.WorkflowDesigner.Flush(); //save the current state of the workflow to the Test() property
+            this.WorkflowDesigner.Flush(); 
             xamlTextBox.Text = this.WorkflowDesigner.Text;
 
+        }
+        
+        #region New-Open-save - Clear
+
+        public void SaveWorkflow()
+        {
+            if (WorkflowDesigner != null && (WorkflowFilePath != @"DefaultWorkflows\defaultWorkflow.xaml"))
+            {
+                WorkflowDesigner.Flush();
+                WorkflowDesigner.Save(WorkflowFilePath);
+            }
+            else
+            {
+                string dummyFileName = "New_WF";
+                SaveFileDialog sf = new SaveFileDialog();
+                // Feed the dummy name to the save dialog
+                sf.Filter = "XAML files(.xaml)|*.xaml";
+                sf.FilterIndex = 2;
+                sf.FileName = dummyFileName;
+
+                if (sf.ShowDialog() == true)
+                {
+                    // Now here's our save folder
+                    string savePath = System.IO.Path.GetDirectoryName(sf.FileName);
+                    
+                    // Do whatever
+                    WorkflowFilePath = sf.FileName;
+                    WorkflowDesigner.Flush();
+                    WorkflowDesigner.Save(WorkflowFilePath);
+
+
+                }
+            }
+        }
+
+        public void NewWorkflow()
+        {
+            ClearWorkflow();
+
+            WorkflowFilePath = @"DefaultWorkflows\defaultWorkflow.xaml";
+
+            AddWorkflowDesigner();
+        }
+
+        public void OpenWorkflow()
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "XAML files(.xaml)|*.xaml";
+
+            if (dlg.ShowDialog() == true)
+            {
+                WorkflowFilePath = dlg.FileName;
+                ClearWorkflow();
+                AddWorkflowDesigner();
+            }
+        }
+
+        public void FastRunWorkflow()
+        {
+            if (!string.IsNullOrEmpty(WorkflowFilePath))
+            {
+                try
+                {
+                    SaveWorkflow();
+                    var writer = new StringWriter();
+                    var workflow = ActivityXamlServices.Load(WorkflowFilePath);
+                    var wa = new WorkflowApplication(workflow);
+                    wa.Extensions.Add(writer);
+                    wa.Run();
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion
+
+        public void ClearWorkflow()
+        {
+            if (WorkflowDesigner != null)
+            {
+                grid1.Children.Remove(WorkflowDesigner.View);
+                grid1.Children.Remove(WorkflowDesigner.PropertyInspectorView);
+                WorkflowDesigner = null;
+            }
         }
 
     }
 }
+
 
